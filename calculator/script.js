26087bbaeb37c0d3011c2579808e763d850f9fbc
Srijan Mondal <90636706+Srijanmondal@users.noreply.github.com>
2024-10-18 22:15:26 +0530

    Create script.js

---
+++ calculator/script.js
@@ -0,0 +1,34 @@
+const display = document.getElementById('display');
+
+
+function appendToDisplay(input){
+  display.value += input;
+}
+function calculate(){
+  display.value = eval(display.value);
+}
+function cleardisplay(){
+  display.value = "";
+}
+function backspace(){
+  display.value = display.value.slice(0,-1);
+}
+
+// construction notice
+const sin = document.getElementById('sin-btn');
+const cos = document.getElementById('cos-btn');
+const tan = document.getElementById('tan-btn');
+
+sin.onclick= function(){
+  appendToDisplay('sin(');
+  display.value = display.value.slice(0,-3);
+display.value= Math.sin(60);
+}
+
+cos.onclick= function(){
+  alert("This function is under construction!")
+}
+tan.onclick= function(){
+  alert("This function is under construction!")
+}
+// notice end
